e8690187f17fa0d703cdc5b6a6cdc59d5fc13b71
Lautaro Decima <decimalautaro@gmail.com>
2023-02-26 22:21:11 -0300

    feat: instalando dependencias de material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {Article} from './components/pages/Article'
 
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
   return (
   <>
   <h1>Blog con react</h1>
+  <Article/>
   
   
   </>

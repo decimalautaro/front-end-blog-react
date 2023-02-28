beefe2cbe7229867be3282d7837e28996bf75c47
Lautaro Decima <decimalautaro@gmail.com>
2023-02-27 23:27:42 -0300

    feat: agregando header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar  position="static" >
+    <Toolbar >
+      <Typography variant="h3">Blog con React</Typography>
+    </Toolbar>
+  </AppBar>
   )
 }

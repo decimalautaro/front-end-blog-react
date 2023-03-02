9d089c43c70959917e9f3f2a5128f7465545e6f5
Lautaro Decima <decimalautaro@gmail.com>
2023-03-01 22:32:19 -0300

    feat: dando estilos a home y completando con contenido

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>Bienvenido a mi blog con react.</h1>
+      <p>Blog desarrollado con MEARN Stack (Mongo, Express, React, Nodejs).</p>
+
+      <Link to="/articles" className='button'>Ver los articulos.</Link>
+    </div>
   )
 }

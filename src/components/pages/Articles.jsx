b184d77d35abaa9d92f165d08d9852e22498f7d7
Lautaro Decima <decimalautaro@gmail.com>
2023-03-03 17:01:37 -0300

    feat: recibiendo articulos desde la api con fetch

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -1,23 +1,51 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 
 export const Articles = () => {
+  const [articles, setArticles] = useState([])
+  useEffect(()=>{
+    getArticles()
+  },[])
+  
+  const getArticles = async()=>{
+    const URL = "http://localhost:3000/api/articles/all-items";
+    let petition = await fetch(URL, {method: "GET"});
+    let data = await petition.json();
+    
+    if (data.status === "success") {
+      setArticles(data.articles)
+    }
+  }
+
   return (
    <>
-    <article className='article-item'>
-      <div className='mask'>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902'/>
-      </div>
-      <div className="datos">
-          <h3 className='title'>Desarrollo web</h3>
-            <p className='description'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod odit ea dolorum veniam iusto illum laborum quasi blanditiis aliquam, ad, ducimus enim officiis error porro atque perferendis fugit fuga.  
-          </p>  
-        <button className='edit'>Editar</button>
-        <button className='delete'>Eliminar</button>
+   {
+   articles.length >= 1 ? (
+   articles.map(article =>{
+      return (
+        <article key={article._id} className='article-item'>
+        <div className='mask'>
+          <img src={article.image}/>
+        </div>
+        <div className="datos">
+            <h3 className='title'>{article.title}</h3>
+              <p className='description'>
+              {article.content}
+            </p>  
+          <button className='edit'>Editar</button>
+          <button className='delete'>Eliminar</button>
+  
+        </div>
+       
+      </article> 
+      )
+   })) :
+   (
+    <h1>NO HAY ARTICULOS</h1>
+   )
+  
+  }
 
-      </div>
-     
-    </article>   
    </> 
  )
 }

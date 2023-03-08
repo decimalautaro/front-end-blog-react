23a8056a0c6b98beeefda3c9ace5886bd5848df6
Lautaro Decima <decimalautaro@gmail.com>
2023-03-08 12:11:53 -0300

    feat: creando formulario de crear articulo

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export const Create = () => {
   return (
-    <div>Create</div>
+    <div className='home'>
+    <h1>Crear articulo.</h1>
+    <form className='form'>
+      <div className='form-grup'>
+        <label  htmlFor='title'>Titulo</label>
+        <input type="text" name="title" />
+      </div>
+
+      <div className='form-grup'>
+        <label  htmlFor='content'>Contenido</label>
+        <textarea type="text" name="content" />
+      </div>
+
+      <div className='form-grup'>
+        <label  htmlFor='file'>Imagen</label>
+        <input type="file" name="file" />
+      </div>
+
+      <input type='submit' value="Guardar" className='btn btn-success'/>
+    </form>
+    </div>
   )
 }

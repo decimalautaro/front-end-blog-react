5453d28270e487e90c399a4267f53c3a09027d58
Lautaro Decima <decimalautaro@gmail.com>
2023-03-09 20:45:19 -0300

    feat: recogiendo datos del formulario

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -3,18 +3,27 @@ import { useState } from 'react'
 import { useForm } from "../../hooks/useForm";
 
 export const Create = () => {
+
+  const {form, send, change} = useForm({})
+
+  const saveArticle = (e)=>{
+    e.preventDefault;
+    let newArticle = JSON.stringify(form);
+  }
+
   return (
     <div className='home'>
     <h1>Crear articulo.</h1>
-    <form className='form'>
+    <pre>{JSON.stringify(form)}</pre>
+    <form className='form' >
       <div className='form-grup'>
         <label  htmlFor='title'>Titulo</label>
-        <input type="text" name="title" />
+        <input type="text" name="title" onChange={change}/>
       </div>
 
       <div className='form-grup'>
         <label  htmlFor='content'>Contenido</label>
-        <textarea type="text" name="content" />
+        <textarea type="text" name="content" onChange={change} />
       </div>
 
       <div className='form-grup'>
